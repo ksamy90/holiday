777d1ee84c090622a2f4a13e7d161e04f3e77d73
ksamy90 <ksamy90@aol.com>
2021-07-15 09:07:50 +0300

    add auth to routes

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -82,7 +82,7 @@ const Auth = () => {
           }
         );
 
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     } else {
       try {
@@ -96,7 +96,7 @@ const Auth = () => {
           "POST",
           formData
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (err) {}
     }
   };
